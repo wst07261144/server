193de319fd9c09b63320885f5bc9668230b987cc
wst07261144 <wst07261144@gmail.com>
2013-12-07 21:02:22 +0800

    简陋的用户资料页面

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'http://ruby.taobao.org'
 ruby '2.0.0'
 gem 'haml'
 gem 'bootstrap-sass', '2.3.2.0'
-gem 'bcrypt-ruby', '3.0.1'
+gem 'bcrypt-ruby',  '3.1.2'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.1'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Server::Application.routes.draw do
 
+  resources :users
 
-  get "users/new"
 
   root to: 'static_pages#home'
+
+
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
