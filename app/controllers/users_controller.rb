d85e26a331ff06cf34e2a219ba605bbd0cc6ef01
wst07261144 <wst07261144@gmail.com>
2013-12-07 22:26:04 +0800

    注册功能实现

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,26 @@
 class UsersController < ApplicationController
   def new
+    @user=User.new
   end
   def show
     @user = User.find params[:id]
+  end
+  def create
+    @user = User.new(user_params)    # Not the final implementation!
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user #转向地址我们直接写了 @user，而没用 user_path，Rails 会自动转向到用户的资料页面
+    else
+      render 'new'
+    end
+  end
+
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
   end
 end
