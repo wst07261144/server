b6fddac5fadec85f0319d1c499e5d0d154298156
wst07261144 <wst07261144@gmail.com>
2013-12-07 17:11:29 +0800

    创建user控制器并在主页添加signup链接

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Server::Application.routes.draw do
 
 
+  get "users/new"
+
   root to: 'static_pages#home'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
+  match '/signup',  to: 'users#new', via: 'get'
 end
