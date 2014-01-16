766f0b731c9c331d0d0ec958550edfb791a38a61
wst07261144 <wst07261144@gmail.com>
2014-01-16 10:36:08 +0800

    用户只能编辑自己的资料

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,10 @@
 class SessionsController < ApplicationController
   def new
+    p '===========new==========='
   end
 
   def create
+    p '=========create==========='
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       sign_in user
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   before_action :signed_in_user, only:[:edit,:update]
+  before_action :correct_user, only: [:edit,:update]
   def new
     @user=User.new
   end
@@ -21,11 +22,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
       redirect_to @user
@@ -43,5 +42,10 @@ class UsersController < ApplicationController
   def signed_in_user
     redirect_to signin_url ,notice:'Please login' if !signed_in?
     #flash[:error] 也可以使用上述的简便方式，但 flash[:success] 却不可以。
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to root_path unless current_user?(@user)
   end
 end
