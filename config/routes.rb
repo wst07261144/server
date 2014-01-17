3acefda8c1349fcc2c796f1cc6ab56d00d733948
wst07261144 <wst07261144@gmail.com>
2014-01-17 15:57:50 +0800

    生成示例数据

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Server::Application.routes.draw do
 
-  resources :users
   resources :sessions, only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
 
   root to: 'static_pages#home'
 
